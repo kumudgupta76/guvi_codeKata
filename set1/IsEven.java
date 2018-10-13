6f8badb393888e7ed069d31d3b6ed4250c7aa47c
kumudorigen <kumudgupta76@gmail.com>
2018-10-13 15:10:25 +0530

    Even no

---
+++ set1/IsEven.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class IsEven{
+    public static void main(String ags[])
+    {
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        if (n%2==0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+        
+    }
+}
