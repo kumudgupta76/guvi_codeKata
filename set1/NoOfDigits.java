f81126f88c02cd2dc944c58e569538585a36ff17
kumudorigen <kumudgupta76@gmail.com>
2018-10-13 15:45:56 +0530

    No of digits

---
+++ set1/NoOfDigits.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+public class NoOfDigits{
+    public static void main(String ags[])
+    {
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        String s=n+"";
+        System.out.println(s.length());
+    }
+}
