201c952d0926660292c5912d7f02a954b540317e
kumudorigen <kumudgupta76@gmail.com>
2018-11-05 13:06:51 +0530

    SET 7 com

---
+++ set7/IsM7_8.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+
+public class IsM7_8{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if (n%7==0)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+    }
+}
